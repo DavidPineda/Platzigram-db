b438c5b1e3017c04c00e977c1903a9894653582d
david pineda <davidpineda1003@gmail.com>
2017-01-26 23:37:25 -0500

    Video 60

--- test/db.test.js
+++ test/db.test.js
@@ -6,30 +6,42 @@ const r = require('rethinkdb')
 const Db = require('../')
 const fixture = require('./fixtures')
 
-const dbName = `platzigram_${uuid.v4()}`
-const db = new Db({db: dbName})
+// test.before('setup database', async t => {
+test.beforeEach('setup database', async t => {
+  let dbName = `platzigram_${uuid.v4()}`
+  let db = new Db({db: dbName})
 
-test.before('setup database', async t => {
   await db.connect()
+  t.context.db = db
+  t.context.dbName = dbName
   t.true(db.connected, 'should be connected')
 })
 
-test.after('disconnect datatbase', async t => {
+// test.after('disconnect datatbase', async t => {
+test.afterEach.always('cleanup datatbase', async t => {
+  let db = t.context.db
+  let dbName = t.context.dbName
+
   await db.disconnect()
   t.false(db.connected, 'should be disconnected')
-})
 
-test.after.always('cleanup database', async t => {
   let conn = await r.connect({})
   await r.dbDrop(dbName).run(conn)
 })
 
+// test.after.always('cleanup database', async t => {
+//   let conn = await r.connect({})
+//   await r.dbDrop(dbName).run(conn)
+// })
+
 test('save image', async t => {
+  let db = t.context.db
+
   t.is(typeof db.saveImage, 'function', 'saveImage is function')
 
   let image = fixture.getImage()
-
   let created = await db.saveImage(image)
+
   t.is(created.description, image.description)
   t.is(created.url, image.url)
   t.is(created.likes, image.likes)
@@ -42,13 +54,39 @@ test('save image', async t => {
 })
 
 test('like image', async t => {
+  let db = t.context.db
+
   t.is(typeof db.likeImage, 'function', 'likeImage is function')
 
   let image = fixture.getImage()
-
   let created = await db.saveImage(image)
   let result = await db.likeImage(created.public_id)
 
   t.true(result.liked)
   t.is(result.likes, image.likes + 1)
+})
+
+test('get image', async t => {
+  let db = t.context.db
+
+  t.is(typeof db.getImage, 'function', 'getImage is function')
+
+  let image = fixture.getImage()
+  let created = await db.saveImage(image)
+  let result = await db.getImage(created.public_id)
+
+  t.deepEqual(created, result)
+})
+
+test('list all images', async t => {
+  let db = t.context.db
+
+  t.is(typeof db.getImages, 'function', 'getImages is function')
+
+  let images = fixture.getImages(5)
+  let saveImages = images.map(img => db.saveImage(img))
+  let created = await Promise.all(saveImages)
+  let result = await db.getImages()
+
+  t.is(created.length, result.length)
 })
